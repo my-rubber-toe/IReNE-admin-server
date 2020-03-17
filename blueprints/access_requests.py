16f7319f75045af4b4f3862df153e5fb8c618a33
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-17 01:13:13 -0400

    Added blueprint for collaborators access requests

---
+++ blueprints/access_requests.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, Response, request
+
+access_req_blueprint = Blueprint('access-requests', __name__, url_prefix='/admin/api/access-request/')
+
+@access_req_blueprint.route('/', methods=['GET'])
+def access_requests():
+    """
+    Retrieve the list of access requests from the database.
+    """
+    pass
+
+@access_req_blueprint.route('/approve', methods=['PUT'])
+def access_requests_approve():
+    """
+    Approve the access request of a user. 
+    """
+    pass
+
+@access_req_blueprint.route('/deny', methods=['DELETE'])
+def access_requests_deny():
+    """
+    Deny the access request of a user. 
+    """
+    pass
