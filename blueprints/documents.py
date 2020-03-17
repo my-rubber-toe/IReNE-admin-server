73182c77c7cb799c142d49a9aea0692da296633f
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-17 02:59:12 -0400

    Added documents blueprints

---
+++ blueprints/documents.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, Response, request
+
+documents_blueprint = Blueprint('documents', __name__, url_prefix='/admin/api/documents')
+
+@documents_blueprint.route('/', methods=['GET'])
+def documents():
+    """
+    Retrieve a list of all the documents in the database.
+    """
+    pass
+
+@documents_blueprint.route('/view/<docID>', methods=['GET'])
+def documents_view(docID):
+    """
+    Retrieve a list of metadata of all the documents in the database.
+    """
+    pass
+
+@documents_blueprint.route('/publish', methods=['PUT'])
+def documents_publish(docID):
+    """
+    Set a document state to be pusblished.
+    """
+    pass
+
+@documents_blueprint.route('/unpublish', methods=['PUT'])
+def documents_publish(docID):
+    """
+    Set a document state to be unpublished.
+    """
+    pass
