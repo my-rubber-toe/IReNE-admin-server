e0eebff0a5cdff793a07b9bbddfc70c045a647f8
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-17 21:54:20 -0400

    refactor(tags.py): changed the name of the blueprint

--- blueprints/tags.py
+++ blueprints/tags.py
@@ -1,15 +1,15 @@
 from flask import Blueprint, Response, request
 
-tags_blueprint = Blueprint('tags', __name__, url_prefix='/admin/api/tags')
+blueprint = Blueprint('tags', __name__, url_prefix='/admin/api/tags')
 
-@tags_blueprint.route('/', methods=['GET'])
+@blueprint.route('/', methods=['GET'])
 def tags():
     """
     Retrieve a list of all the system tags.
     """
     pass
 
-@tags_blueprint.route('/remove', methods=['DELETE'])
+@blueprint.route('/remove', methods=['DELETE'])
 def tags_remove():
     """
     Remove a tag from all documents and the system tags colleciton.
