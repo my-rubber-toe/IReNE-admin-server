e1a1f757e733dca9c0264cf69692be785c266147
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-18 04:40:49 -0400

    feat(documents): implemented all the logic to the routes

--- blueprints/documents.py
+++ blueprints/documents.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response, request
+from utils.responses import ApiResult, ApiException
 
 blueprint = Blueprint('documents', __name__, url_prefix='/admin/api/documents')
 
@@ -7,25 +8,101 @@ def documents():
     """
     Retrieve a list of all the documents in the database.
     """
-    return "Documents"
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    # TODO: Use DAOs to retrieve all the documents.
+    return ApiResult(
+        message='All available documents'
+    )
 
 @blueprint.route('/view/<docID>', methods=['GET'])
 def documents_view(docID):
     """
     Retrieve a list of metadata of all the documents in the database.
     """
-    return "Document view ID: " + str(docID)
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    
+    valid_doc_id = False
+    # TODO: Check if doc id exist
+    if not valid_doc_id:
+        return ApiException(
+            error_type='Not Found',
+            message='The document ID given was not found.',
+            status=404
+        )
+
+    # TODO: Use DAOs to retrieve document.
+    return ApiResult(
+        message='Valid Collaborator Ban'
+    )
 
 @blueprint.route('/publish', methods=['PUT'])
 def documents_publish():
     """
     Set a document state to be pusblished.
     """
-    return "Successful publish"
+    #doc_id  = request.form.get('docID')
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    
+    valid_doc_id = False
+    # TODO: Check if doc id exist
+    if not valid_doc_id:
+        return ApiException(
+            error_type='Not Found',
+            message='The document ID given was not found.',
+            status=404
+        )
+
+    # TODO: Use DAOs to publish document.
+    return ApiResult(
+        message='Valid Document Publish'
+    )
 
 @blueprint.route('/unpublish', methods=['PUT'])
 def documents_unpublish():
     """
     Set a document state to be unpublished.
     """
-    return "Successful unpublish"
+    #doc_id  = request.form.get('docID')
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    
+    valid_doc_id = False
+    # TODO: Check if doc id exist
+    if not valid_doc_id:
+        return ApiException(
+            error_type='Not Found',
+            message='The document ID given was not found.',
+            status=404
+        )
+
+    # TODO: Use DAOs to unpublish document.
+    return ApiResult(
+        message='Valid Document Unpublish'
+    )
