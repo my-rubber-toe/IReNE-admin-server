82b98d88b931da708a035f85827c6e3c68a8da12
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-18 03:14:57 -0400

    feat(access-requests): implemented routes logic

--- blueprints/access_requests.py
+++ blueprints/access_requests.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Response, request
+from utils.responses import ApiResult, ApiException
 
 blueprint = Blueprint('access-requests', __name__, url_prefix='/admin/api/access-request')
 
@@ -7,34 +8,73 @@ def access_requests():
     """
     Retrieve the list of access requests from the database.
     """
+    valid_session_token = False
     # TODO: Check if user has a valid session token.
-    return "Access Request"
-"""
-    try:
-        GetDocumentsValidator().load(request.json)
-    except ValidationError as err:
+    if not valid_session_token:
         return ApiException(
-            error_type='Validation Error',
-            message=err.messages,
-            status=400
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
         )
-
     # TODO: Use DAOs to retrieve the necessary information.
-
     return ApiResult(
         message='Valid Data'
-    )"""
+    )
 
 @blueprint.route('/approve', methods=['PUT'])
 def access_requests_approve():
     """
     Approve the access request of a user. 
     """
-    return "Access Request Approve"
+    #collab_id  = request.form.get('collabID')
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    
+    valid_collab_id = False
+    # TODO: Check if collab id exist
+    if not valid_collab_id:
+        return ApiException(
+            error_type='Not Found',
+            message='The collaborators ID given was not found.',
+            status=404
+        )
+
+    # TODO: Use DAOs to retrieve the necessary information.
+    return ApiResult(
+        message='Valid Data'
+    )
 
 @blueprint.route('/deny', methods=['DELETE'])
 def access_requests_deny():
     """
     Deny the access request of a user. 
     """
-    return "Access Request Deny"
+    #collab_id  = request.form.get('collabID')
+    valid_session_token = False
+    # TODO: Check if user has a valid session token.
+    if not valid_session_token:
+        return ApiException(
+            error_type='Unauthorized',
+            message='Unauthorized access to admin dashboard.',
+            status=401
+        )
+    
+    valid_collab_id = False
+    # TODO: Check if collab id exist
+    if not valid_collab_id:
+        return ApiException(
+            error_type='Not Found',
+            message='The collaborators ID given was not found.',
+            status=404
+        )
+
+    # TODO: Use DAOs to retrieve the necessary information.
+    return ApiResult(
+        message='Valid Data'
+    )
