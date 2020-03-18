8cca1fb695e2f176a1d9896f2198fa43a163e5c1
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-17 21:53:44 -0400

    refactor(documents.py): changed the name of the blueprint

--- blueprints/documents.py
+++ blueprints/documents.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, Response, request
 
-documents_blueprint = Blueprint('documents', __name__, url_prefix='/admin/api/documents')
+blueprint = Blueprint('documents', __name__, url_prefix='/admin/api/documents')
 
-@documents_blueprint.route('/', methods=['GET'])
+@blueprint.route('/', methods=['GET'])
 def documents():
     """
     Retrieve a list of all the documents in the database.
     """
     pass
 
-@documents_blueprint.route('/view/<docID>', methods=['GET'])
+@blueprint.route('/view/<docID>', methods=['GET'])
 def documents_view(docID):
     """
     Retrieve a list of metadata of all the documents in the database.
     """
     pass
 
-@documents_blueprint.route('/publish', methods=['PUT'])
+@blueprint.route('/publish', methods=['PUT'])
 def documents_publish():
     """
     Set a document state to be pusblished.
     """
     pass
 
-@documents_blueprint.route('/unpublish', methods=['PUT'])
+@blueprint.route('/unpublish', methods=['PUT'])
 def documents_unpublish():
     """
     Set a document state to be unpublished.
