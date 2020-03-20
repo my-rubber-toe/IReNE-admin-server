deda991f696a92eb51c31d975d67d171f3b49d41
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-20 04:08:13 -0400

    feat(authentication): implemented authentication procedure

--- blueprints/authentication.py
+++ blueprints/authentication.py
@@ -1,73 +1,67 @@
+from flask import current_app, jsonify
 from flask import Blueprint, Response, request
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, \
+    jwt_required, get_jwt_identity, get_raw_jwt
+from utils.responses import ApiResult, ApiException
+from exceptions.handler import AdminServerApiError, AdminServerAuthError
+from datetime import timedelta
+
+
 
 blueprint = Blueprint('authentication', __name__, url_prefix='/admin/api/auth')
 
+# Set blacklist set for blacklisted tokens
+# TODO: Replace blacklist with a Redis Store
+token_blacklist = set()
 
 
-@blueprint.route('/login', methods=['GET'])
+@current_app.jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    """Verifies if a token has been blacklisted."""
+    jti = decrypted_token['jti']
+    return jti in token_blacklist
+
+
+@blueprint.route('/login')
 def login():
+    """Generate a new access token for the user. User must sign in to the databse to get a valid token.
     """
-    Login the user and establish a new firebase session using a valid firebase token. 
-    If a session already exists, then return an `ApiException` response
-
-    Returns
-    -------
-    Response
-        The flask response object containing the result of a successfull login.
-    """
-
-    # Verify if the session cookie exists and the session is valid
-    """if current_app.session_cookie_name in request.cookies and session_handler.check_valid_session() is not False:
-        return ApiException('A valid session already exists!', _type='Authentication', status=401)
-    
-    try:
-        id_token = request.headers['Authorization'].split(' ')[1]
-        return session_handler.session_login(id_token)
-    except ValueError:
-        return ApiException('Invalid token!', _type='Authentication', status=401)"""
-    pass
+    username_exists = True
+    username = request.form.get("username")
+    #TODO: Check if admin username exists in DB
+    if not username_exists:
+        raise AdminServerAuthError(
+            msg= 'Unauthorized access to admin dashboard.', 
+            status = 401
+            )
+    # Use the username as the token identity
+    return ApiResult(
+        access_token=create_access_token(identity=username, expires_delta=timedelta(hours=1)),
+        refresh_token=create_refresh_token(identity=username, expires_delta=timedelta(weeks=2))
+    )
 
 
-@blueprint.route('/logout', methods=['GET'])
+@blueprint.route('/me')
+@jwt_required
+def me():
+    """"Return information from the database."""
+    # TODO: Use DAOs to look for user in the database.
+    return ApiResult(identity=get_jwt_identity())
+
+
+@blueprint.route('/refresh', methods=["GET"])
+@jwt_refresh_token_required
+def refresh():
+    """Return a new access_token given a valid refresh token."""
+    username = get_jwt_identity()
+    return ApiResult(access_token=create_access_token(identity=username, expires_delta=timedelta(hours=2)))
+
+
+@blueprint.route("/logout")
+@jwt_required
 def logout():
-    """
-    Logout the user and by setting the user session invalid and revoking all tokens from the user.
-
-    Returns
-    -------
-    Response
-        The flask response object containing the result of a successfull logout.
-    """
-    pass
-    #return session_handler.session_logout()
-
-
-@blueprint.route('/verify', methods=['GET'])
-def verify():
-    """
-    Verify user sessions and return session informatio
-
-    Returns
-    -------
-    Response
-        The flask response object containing a valid `dict` with the session information 
-
-    """
-    pass
-    """return ApiResult({
-        'message': 'TO-DO: Verify email!',
-    })"""
-
-@blueprint.route('/register', methods=['POST'])
-def register_user():
-    """ Registers new user to the database"""
-    pass
-    #return user_handler.register_new_user(request.json)
-
-@blueprint.before_request
-def authentication_before_req():
-    """
-    `Before request` function for the `admin/api/auth/` routes to validate the sessions and token
-    information. If a valid session is found, the session token is refreshed.
-    """
-    pass
+    """Revoke the Google authorization and add tokens to blacklist"""
+    # Blacklist jwt tokens
+    jti = get_raw_jwt()['jti']
+    token_blacklist.add(jti)
+    return ApiResult(message="Successfully logged out.")
