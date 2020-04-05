69a803301a7513dc164e34cc014c2e0fc414c5ae
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-04-05 16:23:22 -0400

    feat(dao): implemented logic of the routes with dummy data and fake daos

--- blueprints/access_requests.py
+++ blueprints/access_requests.py
@@ -3,8 +3,10 @@ from flask_jwt_extended import get_jwt_identity, fresh_jwt_required
 from utils.responses import ApiResult, ApiException
 from utils.validators import ObjectID
 from exceptions.handler import AdminServerApiError, AdminServerAuthError
+from daos.access_requests import AccessRequests
 
 blueprint = Blueprint('access-requests', __name__, url_prefix='/admin/access-request')
+dao = AccessRequests()
 
 @blueprint.route('/', methods=['GET'])
 @fresh_jwt_required
@@ -13,8 +15,9 @@ def access_requests():
     Retrieve the list of access requests from the database.
     """
     # TODO: Use DAOs to retrieve the necessary information.
+    requests = dao.get_access_requests()
     return ApiResult(
-        message='Valid Data'
+        body={'requests':requests}
     )
 
 @blueprint.route('/approve', methods=['PUT'])
@@ -24,52 +27,45 @@ def access_requests_approve():
     Approve the access request of a user. 
     """
     collab_id  = request.form.get('collabID')
-    #valid_collab_id = ObjectID().is_valid(collab_id)
-    valid_collab_id = True
+    valid_collab_id = ObjectID().is_valid(collab_id)
     if not valid_collab_id:
         raise AdminServerApiError(
-            msg='The collaborators ID given was not validated.',
+            msg='The collaborators ID given is not valid.',
             status=400
         )
-    collab_id_exist = True
-    # TODO: Check if collab id exist
-    if not collab_id_exist:
+    access_request = dao.accept_access_request(collab_id)
+    if access_request is None:
         raise AdminServerApiError(
             msg='The collaborators ID given was not found.',
             status=404
         )
 
     # TODO: Use DAOs to retrieve the necessary information.
-    return ApiResult(
-        message='Valid Data',
-        collabID = collab_id
+    return ApiResult(body = 
+        {'access_request': access_request}
     )
 
-@blueprint.route('/deny', methods=['DELETE'])
+@blueprint.route('/deny', methods=['PUT'])
 @fresh_jwt_required
 def access_requests_deny():
     """
     Deny the access request of a user. 
     """
     collab_id  = request.form.get('collabID')
-    #valid_collab_id = ObjectID().is_valid(collab_id)
-    valid_collab_id = True
+    valid_collab_id = ObjectID().is_valid(collab_id)
     if not valid_collab_id:
         raise AdminServerApiError(
-            msg='The collaborators ID given was not validated.',
+            msg='The collaborators ID given is not valid.',
             status=400
         )
-    # TODO: Check if collab id exist
-    collab_id_exist = True
-    # TODO: Check if collab id exist
-    if not collab_id_exist:
-        return AdminServerApiError(
+    access_request = dao.deny_access_request(collab_id)
+    if access_request is None:
+        raise AdminServerApiError(
             msg='The collaborators ID given was not found.',
             status=404
         )
 
     # TODO: Use DAOs to retrieve the necessary information.
     return ApiResult(
-        message='Valid Data',
-        collabID = collab_id
+        body = {'access_request': access_request}
     )
---
+++ daos/dummy_data/access_request.py
@@ -0,0 +1,39 @@
+requests  = [
+  {"_id": '5e89d57b3e1e4ef361ce68ef', 'documentsID': ['5e89dd1077c9a2c523f90179', '5e89e9e1664514d30949fb14'], 
+  'firstName': 'Roberto', 'last_name': 'Guzman', 'email': 'roberto.guzman3@upr.edu', 'banned': True, 'faculty': 'INEL', 'approved': True},
+
+  {'_id': '5e89d58b2ac039eb9ed2ebc9', 'documentsID': ['5e89dd288593ec9607e503ea'], 
+  'firstName': 'Yomar', 'last_name': 'Ruiz', 'email': 'yomar.ruiz@upr.edu', 'banned': False, 'faculty': 'INCI', 'approved': True},
+
+  {'_id': '5e89d597025ce32be041994a', 'documentsID': ['5e89dd2f258b577c59e2b1ba'], 
+  'firstName': 'Jainel', 'last_name': 'Torres', 'email': 'jainel.torrer@upr.edu', 'banned': False, 'faculty': 'INEL', 'approved': True},
+
+  {'_id': '5e89d59fe1925dad4708fd2f', 'documentsID': ['5e89e88ab06760a6297fa39e'], 
+  'firstName': 'Alberto', 'last_name': 'Canela', 'email': 'alberto.canela@upr.edu', 'banned': False, 'faculty': 'CIIC', 'approved': True},
+
+  {'_id': '5e89d5a83814cce58bf71036', 'documentsID': ['5e89e893ebd8eb9cc98f744f'], 
+  'firstName': 'Alejandro', 'last_name': 'Vasquez', 'email': 'alejandro.vasquez@upr.edu', 'banned': False, 'faculty': 'INQU', 'approved': True},
+
+  {'_id': '5e89d5b1cb472937a4eb1f01', 'documentsID': ['5e89e95ae4f48b88da892d8f'], 
+  'firstName': 'Don', 'last_name': 'Quijote', 'email': 'don.quijote@upr.edu', 'banned': True, 'faculty': 'ININ', 'approved': True},
+
+#Access Requests == approved flag to false
+
+  {"_id": '5e89d5bc967dc3fab8d85c9f', 'documentsID': [''], 
+  'firstName': 'Sancho', 'last_name': 'Panza', 'email': 'sancho.panza@upr.edu', 'banned': True, 'faculty': 'ICOM', 'approved': False},
+
+  {'_id': '5e89d5c4c1f6abd06ac4a10d', 'documentsID': [''],
+  'firstName': 'Dulcinea', 'last_name': 'del Toboso', 'email': 'dulcinea.deltoboso@upr.edu', 'banned': False, 'faculty': 'ICOM', 'approved': False},
+
+  {'_id': '5e89d5cc3826bf00216d640c', 'documentsID': [''], 
+  'firstName': 'Quijote', 'last_name': 'de la Mancha', 'email': 'quijote.delamancha@upr.edu', 'banned': False, 'faculty': 'ICOM', 'approved': False},
+
+  {'_id': '5e89d5d3069400613be84f37', 'documentsID': [''], 
+  'firstName': 'Dante', 'last_name': 'Alighieri', 'email': 'dante.alighieri@upr.edu', 'banned': False, 'faculty': 'ICOM', 'approved': False},
+
+  {'_id': '5e89d5dbdc82ec47a5024d9a', 'documentsID': [''], 
+  'firstName': 'William', 'last_name': 'Shakespeare', 'email': 'william.shakespeare@upr.edu', 'banned': False, 'faculty': 'ICOM', 'approved': False},
+
+  {'_id': '5e89d5e276f5c8e4d9979ae6', 'documentsID': [''], 
+  'firstName': 'Julius', 'last_name': 'Ceasar', 'email': 'julius.ceasar@upr.edu', 'banned': True, 'faculty': 'ICOM', 'approved': False}
+]
