9ac65f860b11c0720e0a8889bd76ea5254f0d09f
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-04-06 11:13:58 -0400

    feat(authentication): implemented authentication with dummy database

--- blueprints/authentication.py
+++ blueprints/authentication.py
@@ -6,11 +6,12 @@ from utils.responses import ApiResult, ApiException
 from exceptions.handler import AdminServerAuthError
 from cachetools import TTLCache
 from datetime import timedelta
+from daos.admin import AdminDAO
 
 
 
 blueprint = Blueprint('authentication', __name__, url_prefix='/admin/auth')
-
+dao = AdminDAO()
 # Set blacklist set for blacklisted tokens
 # TODO change ttl to the decided token ttl
 token_blacklist = TTLCache(maxsize=10000, ttl=600)
@@ -19,7 +20,7 @@ token_blacklist = TTLCache(maxsize=10000, ttl=600)
 @current_app.jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token):
     """Verifies if a token has been blacklisted."""
-    jti = decrypted_token['jti']
+    jti = decrypted_token['jti'] 
     if token_blacklist.currsize == 0:
         return False
     entry = token_blacklist.get(jti)  #search for the jti on the blacklist#
@@ -30,17 +31,33 @@ def check_if_token_in_blacklist(decrypted_token):
 def login():
     """Generate a new access token for the user. User must sign in to the databse to get a valid token.
     """
-    username_exists = True
     username = request.form.get("username")
-    #TODO: Check if admin username exists in DB
-    if not username_exists:
+    password = request.form.get("password")
+    if username is None or username=='':
+        raise AdminServerAuthError(
+            msg= 'Username field is empty.', 
+            status = 400
+            )
+    if password is None or password=='':
+        raise AdminServerAuthError(
+            msg= 'Password field is empty.', 
+            status = 400
+            )
+    admin = dao.get_admin(username)
+    if not admin:
+        raise AdminServerAuthError(
+            msg= 'Unauthorized access to admin dashboard.', 
+            status = 401
+            )
+    authorized = dao.check_password(admin['password_hash'], password)
+    if not authorized:
         raise AdminServerAuthError(
             msg= 'Unauthorized access to admin dashboard.', 
             status = 401
             )
     # Use the username as the token identity
-    return ApiResult(
-        access_token=create_access_token(identity=username, fresh = timedelta(hours=1))
+    return ApiResult(body =
+        {'access_token':create_access_token(identity=username, fresh = timedelta(hours=1))}
     )
 
 
@@ -49,7 +66,9 @@ def login():
 def me():
     """"Return information from the database."""
     # TODO: Use DAOs to look for user in the database.
-    return ApiResult(identity=get_jwt_identity())
+    return ApiResult(body = 
+    {'identity':get_jwt_identity()}
+    )
 
 
 @blueprint.route('/refresh', methods=["GET"])
@@ -57,7 +76,9 @@ def me():
 def get_fresh():
     """Return a new access_token given a valid refresh token."""
     username = get_jwt_identity()
-    return ApiResult(access_token=create_access_token(identity=username, fresh=timedelta(hours=1)))
+    return ApiResult(body = 
+    {'access_token':create_access_token(identity=username, fresh=timedelta(hours=1))}
+    )
 
 
 @blueprint.route("/logout")
@@ -68,4 +89,6 @@ def logout():
     jti = get_raw_jwt()['jti']
     token_blacklist[jti] = True   # Add the jti to the cache with value true #
     print(token_blacklist)
-    return ApiResult(message="Successfully logged out.")
+    return ApiResult(body = 
+    {'message':"Successfully logged out."}
+    )
---
+++ daos/dummy_data/admin.py
@@ -0,0 +1,4 @@
+admins = [
+    {"_id": '5e89d66481d1166cedcec75c','username':'yomar.ruiz', 'password_hash': '$2a$04$x7SLUHC/pcdhQsRZbpt.V.v0twKPCMQEcg2xqryytH6dzF6wapCra'},
+    {"_id": '5e89d66c5fe884a71bd0a05f', "username": 'sancho.panza', 'password_hash': '$2a$04$r3wZ7pnXe6XGIrmeG./bQOtiZysbf8w/wx0NVQpRatXKvJvt.tqa6'}
+]
